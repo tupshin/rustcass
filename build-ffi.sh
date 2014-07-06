10fd36f1646e08a5b81050cf51741e9153530568
Tupshin Harper <tupshin@tupshin.com>
2014-07-05 23:41:55 -0700

    Update build-ffi.sh

--- build-ffi.sh
+++ build-ffi.sh
@@ -1,1 +1,1 @@
-LD_PRELOAD=/usr/lib/llvm-3.4/lib/libclang.so ../rust-bindgen/bindgen -l cassandra -match cassandra.h -o cassandra.rs /usr/local/include/cassandra.h 
+LD_PRELOAD=/usr/lib/llvm-3.4/lib/libclang.so ../rust-bindgen/bindgen -l cassandra -match cassandra.h -o src/cassandra.rs /usr/local/include/cassandra.h 
